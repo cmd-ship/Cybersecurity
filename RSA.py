60e3d803af945fdca0e57cbd152598be26c0fdbc
Robin Lockwood <rlockwood@localhost.localdomain>
2019-04-12 16:53:04 -0600

    Implemented chinese remainder theorem.

--- RSA.py
+++ RSA.py
@@ -1,57 +1,68 @@
 import random
 
-"""
-Do not use this script for production.
-Python does not adhere to encapsulation and all variables are therefore accessible outside of class hierarchy.
-This means anyone could access variable that would otherwise be private, like private keys!!!!
-"""
-
 
 class Person:
 
     def __init__(self):
         self.public_key = None
-        self.e = None
-        self.d = None
+        self.private_key = None
 
     def encrypt(self, m):
-        return pow(m, self.e) % self.public_key
+        return pow(m, self.public_key[1]) % self.public_key[0]
 
     def decrypt(self, c):
-        return pow(c, self.d) % self.public_key
+        return pow(c, self.private_key[1]) % self.private_key[0]
 
-    def generate_keys(self):
+    def decrypt_crt(self, c):
+        dp, dq, qinv, p, q = self.private_key
+        x1 = pow(c, dp) % p
+        x2 = pow(c, dq) % q
+
+        h = qinv * (x1-x2) % p
+
+        return x2 + (h * q)
+
+    def generate_keys(self, crt=False):
         # choose two prime numbers p and q
-        p = random.randint(0, 100)
-        while miller_rabin(p, 10) is False:
-            p = random.randint(0, 100)
-        q = random.randint(0, 100)
-        while q != p and miller_rabin(q, 10) is False:
-            q = random.randint(0, 100)
-        self.public_key = p*q
-        while self.get_multiplicative_inverse(p, q) is False:
+        p = random.randint(0, 50)
+        while miller_rabin(p, 500) is False:
+            p = random.randint(0, 50)
+        q = random.randint(0, 50)
+        while q != p and miller_rabin(q, 500) is False:
+            q = random.randint(0, 50)
+        while self.get_multiplicative_inverse(p, q, crt) is False:
             continue
 
-    def get_multiplicative_inverse(self, p, q):
+    def get_multiplicative_inverse(self, p, q, crt=False):
+
         m_lambda = least_common_multiple(p - 1, q - 1)
+
+        # compute an e that is coprime to m_lambda
         e = random.randint(2, m_lambda)
         # choose e such that 1 < e < m_lambda
-        while miller_rabin(e, 20) is False and m_lambda % e == 0:
+        while m_lambda % e == 0 or miller_rabin(e, 500) is False:
             # e should be coprime -> generate primes, it shouldn't divide m_lambda
             e = random.randint(2, m_lambda)
         if e >= m_lambda:
             return False
-        if gcd(e, m_lambda) != 1 or (p - q) // 2.0 == 0:
+        if int((p - q) / 2.0) == 0 or gcd(e, m_lambda) != 1:
             return False
-        self.e = e
-        self.d = mod_inverse(e, m_lambda)
+        self.public_key = (p*q, e)
+        self.private_key = (p*q, mod_inverse(e, m_lambda))
+
+        if crt is True:
+            d_p = self.private_key[1] % (p - 1)
+            d_q = self.private_key[1] % (q - 1)
+            q_inv = mod_inverse(q, p)
+            self.private_key = (d_p, d_q, q_inv, p, q)
+
         return True
 
     def get_public_key(self):
-        return self.public_key, self.e
+        return self.public_key
 
     def receive_key(self, pub_key):
-        self.public_key, self.e = pub_key
+        self.public_key = pub_key
 
 
 # Miller-Rabin primality test
@@ -65,15 +76,15 @@ def miller_rabin(n, k):
     d = n - 1
     while d % 2 == 0:
         d /= 2.
-
     for i in range(k):
         if miller_test(n, d) is False:
             return False
+
     return True
 
 
 def miller_test(n, d):
-    a = random.randint(2, n-2)
+    a = random.randint(3, n-2)
     x = pow(a, d) % n
 
     if x == 1 or x == n-1:
@@ -112,21 +123,48 @@ def mod_inverse(a, m):
     return 1
 
 
-# Don't let anyone see!!! *Remove from script before using to avoid exposing keys*
 if __name__ == "__main__":
-    Arlen = Person()
-    Nancy = Person()
-    Arlen.generate_keys()
-    print(Arlen.public_key, Arlen.d, Arlen.e)
-    Nancy.receive_key(Arlen.get_public_key())
-    print(Nancy.public_key, Nancy.d, Nancy.e)
-    M = [ord(letter) for letter in "Hello, World!"]
-    cipher = [Nancy.encrypt(m) for m in M]
-    plain_text = [Arlen.decrypt(c) for c in cipher]
-    print([chr(ch) for ch in cipher])
-    pt = [chr(ch) for ch in plain_text]
-    text = ""
-    for c in pt:
-        text += c
-    print(text)
+    # Arlen = Person()
+    # Nancy = Person()
+    # Arlen.generate_keys()
+    #
+    # print(Arlen.public_key, Arlen.private_key)
+    #
+    # Nancy.receive_key(Arlen.get_public_key())
+    #
+    # print(Nancy.public_key, Nancy.private_key)
+    #
+    # M = [ord(letter) for letter in "Hello, World!"]
+    # cipher = [Nancy.encrypt(m) for m in M]
+    # plain_text = [Arlen.decrypt(c) for c in cipher]
+    #
+    # print([chr(ch) for ch in cipher])
+    # pt = [chr(ch) for ch in plain_text]
+    # text = ""
+    # for c in pt:
+    #     text += c
+    # print(text)
 
+    # Nancy.generate_keys(crt=True)
+    #
+    # Arlen.receive_key(Nancy.get_public_key())
+    #
+    # print(Nancy.public_key, Nancy.private_key)
+    # print(Arlen.public_key, Arlen.private_key)
+    #
+    # M = [ord(letter) for letter in "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."]
+    # cipher = [Arlen.encrypt(m) for m in M]
+    # plain_text = [Nancy.decrypt_crt(c) for c in cipher]
+    #
+    # print([chr(ch) for ch in cipher])
+    # pt = [chr(int(ch)) for ch in plain_text]
+    # text = ""
+    # for c in pt:
+    #     text += c
+    # print(text)
+
+    primes = ([miller_rabin(n, 100000) for n in range(0, 50)])
+
+    for i in range(0, 50):
+        if primes[i]:
+            print(i)
